31c9b33043594285e0fe363bd8c1027f697e8c84
Abdelkader Bouallaga <babdelka@student.1337.ma>
2022-12-10 00:56:48 +0100

    update

--- src/ft_ssl.c
+++ src/ft_ssl.c
@@ -39,8 +39,11 @@ unsigned long long pow(int i, int j)
     return (result);
 }
 
+
+
 int md5(char *str)
 {
+    // Append padding bits and Append length
     // unsigned int len;
     unsigned long long len;
     unsigned long long temp;
@@ -65,29 +68,34 @@ int md5(char *str)
     char *buffer = (char *)malloc(sizeof(char) * newlen+(64/8));
     ft_bzero(buffer, newlen);
     ft_memcpy(buffer, str, len);
-    printf("64 bit l: ");
+    printf("64 bit l: \n");
     // ft_memcpy(buffer+newlen, *len, 8);
-    int i = 0;
-    int j = 0;
-    int p = 63;
+
+    int i = 0; // count the bits
+    int p = 63; // navigate through the 64 bits value
+    int j = 0; // navigate through the buffer blocks
+    int bit = 8; // set the bits in the buffer blocks
     while (1){
         // printf("2^%d: %llu\n",p, pow(2,p));
         if(pow(2,p) <= messageBits_bak){
-            printf("1");
-            buffer[newlen+j] = buffer[newlen+j]|(1<<p-(7-j));
-            messageBits_bak -= pow(2,p);
+            // printf("1 | ");
+            buffer[newlen+j] = buffer[newlen+j]|(1<<bit); // set the bit in the buffer to 1
+            messageBits_bak -= pow(2,p); // subtract the value of the bit from the messageBits
             // len -= pow(2,p);
         }
-        else{
-            printf("0");
-        }
+        // else{
+        //     printf("0 | ");
+        // }
+        // printf("j = %d, bit = %d\n",j,bit);
         i++;
         if (i == 8){
-            printf(" ");
+            // printf("\n");
             i = 0;
+            bit = 8;
             j++;
         }
         p--;
+        bit--;
         if (p == -1)
             break;
     }
@@ -96,6 +104,28 @@ int md5(char *str)
     buffer[len] = 0x80;
     printf("newmessageBits: %llu\n",messageBits);
     displaybits(buffer, newlen+8);
+    // Initialize MD Buffer
+    u_int32_t a0 = 0x67452301;
+    u_int32_t b0 = 0xefcdab89;
+    u_int32_t c0 = 0x98badcfe;
+    u_int32_t d0 = 0x10325476;
+    // Process message in 512 bit (16-word) blocks
+    printf("\n");
+
+    i = 0;
+    j = 0;
+    u_int32_t *m[16];
+    while(i < newlen+8){
+        ft_memcpy(&m[j], &buffer[i], 4);
+        m[j] = &buffer[i];
+        displaybits(m[j], 4);
+        i+=4;
+        j++;
+        if (j == 4){
+            printf("\ni = %d\n",i);
+            j = 0;
+        }
+    } 
     return(0);
 }
 
